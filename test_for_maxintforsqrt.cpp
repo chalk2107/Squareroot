aab3781d6c1bb05079521f410a27e7221251df92
Starikov Ilya <cubic210705@gmail.com>
2025-06-05 23:12:33 +0300

    Изменил тесты с учетом числа через вектор для функции по обработке группы цифр

--- test_for_maxintforsqrt.cpp
+++ test_for_maxintforsqrt.cpp
@@ -1,73 +1,73 @@
 #include "test_for_maxintforsqrt.h"
 
 void Test_for_maxIntforSqRt::minimumNumber() {
-    int p = 0;
+    QVector<char> p = {'0'};
     QVector<char> vector = {'1'};
     int x = 0;
-    int sgrupInt = 0;
     int lenSgrupInt = 1;
 
-    QVERIFY2(maxIntforSqRt(p, vector, x, sgrupInt, lenSgrupInt) == 1, "Функция вернула неверный результат y");
+    QVector<char> exp_vector = {'1'};
+
+    QVERIFY2(maxIntforSqRt(p, vector, x, lenSgrupInt) == exp_vector, "Функция вернула неверный результат y");
     QVERIFY2(x == 1, "Неверное значение x");
-    QVERIFY2(sgrupInt == 1, "Неверное значение sgrupInt");
 }
 
 void Test_for_maxIntforSqRt::singleDigitNumberOutcomeInteger() {
-    int p = 0;
+    QVector<char> p = {'0'};
     QVector<char> vector = {'9'};
     int x = 0;
-    int sgrupInt = 0;
     int lenSgrupInt = 1;
 
-    QVERIFY2(maxIntforSqRt(p, vector, x, sgrupInt, lenSgrupInt) == 9, "Функция вернула неверный результат y");
+    QVector<char> exp_vector = {'9'};
+
+    QVERIFY2(maxIntforSqRt(p, vector, x, lenSgrupInt) == exp_vector, "Функция вернула неверный результат y");
     QVERIFY2(x == 3, "Неверное значение x");
-    QVERIFY2(sgrupInt == 9, "Неверное значение sgrupInt");
 }
 
 void Test_for_maxIntforSqRt::twoDigitNumberOutcomeSmaller() {
-    int p = 0;
+    QVector<char> p = {'0'};
     QVector<char> vector = {'2', '4'};
     int x = 0;
-    int sgrupInt = 0;
     int lenSgrupInt = 2;
 
-    QVERIFY2(maxIntforSqRt(p, vector, x, sgrupInt, lenSgrupInt) == 16, "Функция вернула неверный результат y");
+    QVector<char> exp_vector = {'1', '6'};
+
+    QVERIFY2(maxIntforSqRt(p, vector, x, lenSgrupInt) == exp_vector, "Функция вернула неверный результат y");
     QVERIFY2(x == 4, "Неверное значение x");
-    QVERIFY2(sgrupInt == 24, "Неверное значение sgrupInt");
 }
 
 void Test_for_maxIntforSqRt::twoDigitNumberOutcomeInteger() {
-    int p = 0;
+    QVector<char> p = {'0'};
     QVector<char> vector = {'8', '1'};
     int x = 0;
-    int sgrupInt = 0;
     int lenSgrupInt = 2;
 
-    QVERIFY2(maxIntforSqRt(p, vector, x, sgrupInt, lenSgrupInt) == 81, "Функция вернула неверный результат y");
+    QVector<char> exp_vector = {'8', '1'};
+
+    QVERIFY2(maxIntforSqRt(p, vector, x, lenSgrupInt) == exp_vector, "Функция вернула неверный результат y");
     QVERIFY2(x == 9, "Неверное значение x");
-    QVERIFY2(sgrupInt == 81, "Неверное значение sgrupInt");
 }
 
 void Test_for_maxIntforSqRt::twoDigitNumberOutcomeSmallerWithPartialRoot() {
-    int p = 2;
+    QVector<char> p = {'2'};
     QVector<char> vector = {'5', '4'};
     int x = 0;
-    int sgrupInt = 0;
     int lenSgrupInt = 2;
 
-    QVERIFY2(maxIntforSqRt(p, vector, x, sgrupInt, lenSgrupInt) == 41, "Функция вернула неверный результат y");
+    QVector<char> exp_vector = {'4', '1'};
+
+    QVERIFY2(maxIntforSqRt(p, vector, x, lenSgrupInt) == exp_vector, "Функция вернула неверный результат y");
     QVERIFY2(x == 1, "Неверное значение x");
-    QVERIFY2(sgrupInt == 54, "Неверное значение sgrupInt");
 }
 
 void Test_for_maxIntforSqRt::threeDigitNumberOutcomeIntegerWithPartialRoot() {
-    int p = 5;
+    QVector<char> p = {'5'};
     QVector<char> vector = {'5', '2', '5'};
     int x = 0;
-    int sgrupInt = 0;
     int lenSgrupInt = 3;
 
-    QVERIFY2(maxIntforSqRt(p, vector, x, sgrupInt, lenSgrupInt) == 525, "Функция вернула неверный результат y");
+    QVector<char> exp_vector = {'5', '2', '5'};
+
+    QVERIFY2(maxIntforSqRt(p, vector, x, lenSgrupInt) == exp_vector, "Функция вернула неверный результат y");
     QVERIFY2(x == 5, "Неверное значение x");
-    QVERIFY2(sgrupInt == 525, "Неверное значение sgrupInt");
 }
